70b7ab6cbbf918fa2d59e12303b5eb578237e9c1
qiqizheng <417542931@qq.com>
2022-12-19 20:24:34 +0800

    init

---
+++ congratulation.c
@@ -0,0 +1,1111 @@
+
+
+#include<stdio.h>
+
+#include<windows.h>
+//
+#define stoptimeshort 40
+
+#define stoptimelong 100
+
+
+
+int main(){
+
+
+
+system("color 1");Sleep(stoptimelong);printf(" * * ** ** * *\n");
+
+system("color 2");Sleep(stoptimelong);printf(" * * ** * * * * * *\n");
+
+system("color 3");Sleep(stoptimelong);printf(" * * * * * * * * * *\n");
+
+system("color 4");Sleep(stoptimelong);printf(" **** * * *** *** ** \n");
+
+system("color 5");Sleep(stoptimelong);printf(" * * * * * * * \n");
+
+system("color 6");Sleep(stoptimelong);printf(" * * ** * * * * \n");
+
+system("color 7");Sleep(stoptimelong);printf(" * * * * * \n");
+
+
+
+system("color 2");Sleep(stoptimelong);printf("\n");
+
+
+
+system("color 8");Sleep(stoptimelong);printf(" *** * * * \n");
+
+system("color 9");Sleep(stoptimelong);printf(" * * * * * * \n");
+
+system("color 1");Sleep(stoptimelong);printf(" * * * * *** \n");
+
+system("color 1");Sleep(stoptimelong);printf(" * * ***** * \n");
+
+system("color 2");Sleep(stoptimelong);printf(" * * * * * \n");
+
+system("color 3");Sleep(stoptimelong);printf(" *** * * * \n");
+
+
+
+system("color 1");Sleep(stoptimelong);printf("\n");
+
+
+
+system("color 2");Sleep(stoptimelong);printf(" CCC Y Y TTTTTTT\n");
+
+system("color 3");Sleep(stoptimelong);printf(" C C Y Y T \n");
+
+system("color 4");Sleep(stoptimelong);printf(" C Y Y T \n");
+
+system("color 5");Sleep(stoptimelong);printf(" C YYY T \n");
+
+system("color 6");Sleep(stoptimelong);printf(" C Y T \n");
+
+system("color 7");Sleep(stoptimelong);printf(" C C Y T \n");
+
+system("color 8");Sleep(stoptimelong);printf(" CCC Y T \n");
+
+
+
+printf("\n");
+
+
+
+system("title 龙城飞将");
+
+
+
+
+
+
+
+printf("%s", " "); system("color 3");
+
+
+
+system("color 9");Sleep(400);printf("%s", " 郑");
+
+
+
+system("color 6");Sleep(400);printf("%s", "琪");
+
+
+
+// system("color 1");Sleep(400);printf("%s", "廷\n");
+
+
+
+system("color 3");Sleep(400);printf("%s", " ");
+
+
+
+printf("%s", " ");system("color 4");
+
+
+
+Sleep(400);printf("%s","今");
+
+
+
+system("color 2");Sleep(400);printf("%s","天");
+
+
+
+system("color 5");Sleep(400);printf("%s","是");
+
+
+
+system("color 9");Sleep(400);printf("%s", "个");
+
+
+
+system("color 6");Sleep(400);printf("%s", "特");
+
+
+
+system("color 1");Sleep(400);printf("%s", "殊");
+
+
+
+system("color 3");Sleep(400);printf("%s", "的");
+
+
+
+system("color 4");Sleep(400);printf("%s", "日");
+
+
+
+system("color 2");Sleep(400);printf("%s", "子\n");
+
+
+
+Sleep(400);printf("%s", " ");
+
+
+
+printf("%s", " ");system("color 5");
+
+
+
+Sleep(400);printf("%s", "2022年 ");
+
+
+
+system("color 9");Sleep(400);printf("%s", "07月 ");
+
+
+
+system("color 6");Sleep(400);printf("%s", "03日\n");
+
+
+
+system("color 1");Sleep(400);printf("%s", " ");
+
+
+
+printf("%s", " ");system("color 3");
+
+
+
+Sleep(stoptimelong);printf("%s", "soga!\n ");
+
+
+
+printf("%s", " "); system("color 4");
+
+
+
+Sleep(1000);printf("%s", "你");
+
+
+
+system("color 2");Sleep(1000);printf("%s", "的");
+
+
+
+system("color 5");Sleep(1000);printf("%s", "出");
+
+
+
+system("color 9");Sleep(1000);printf("%s", "厂");
+
+
+
+system("color 6");Sleep(1000);printf("%s", "日");
+
+
+
+system("color 1");Sleep(1000);printf("%s", "期");
+
+
+
+system("color 3");Sleep(1000);printf("%s", "喔\n");
+
+
+
+Sleep(1000);
+
+
+
+Sleep(500);printf("%s", " ");
+
+
+
+printf("%s", " ");system("color 4");
+
+
+
+Sleep(300);printf("%s", "你");
+
+
+
+system("color 2");Sleep(300);printf("%s", "还");
+
+
+
+system("color 5");Sleep(300);printf("%s", "是");
+
+
+
+system("color 9");Sleep(300);printf("%s", "一");
+
+
+
+system("color 6");Sleep(300);printf("%s", "如");
+
+
+
+system("color 1");Sleep(300);printf("%s", "既");
+
+
+
+system("color 3");Sleep(300);printf("%s", "往");
+
+
+
+system("color 4");Sleep(300);printf("%s", "的");
+
+
+
+system("color 2");Sleep(300);printf("%s", "少");
+
+
+
+system("color 5");Sleep(300);printf("%s", "年");
+
+
+
+system("color 9");Sleep(300);printf("%s", "鲜");
+
+
+
+system("color 6");
+
+
+
+Sleep(300);printf("%s", "衣");
+
+
+
+system("color 1");Sleep(300);printf("%s", "怒");
+
+
+
+system("color 3");Sleep(300);printf("%s", "马 ");
+
+
+
+system("color 4");Sleep(300);printf("%s", "踏");
+
+
+
+system("color 2");Sleep(300);printf("%s", "着");
+
+
+
+system("color 5");Sleep(300);printf("%s", "凌");
+
+
+
+system("color 9");Sleep(300);printf("%s", "云\n");
+
+
+
+system("color 6");Sleep(300);printf("%s", " ");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 1");Sleep(300);printf("%s", "愿");
+
+
+
+system("color 3");Sleep(300);printf("%s", "你");
+
+
+
+system("color 4");Sleep(300);printf("%s", "一");
+
+
+
+system("color 2");Sleep(300);printf("%s", "生");
+
+
+
+system("color 5");Sleep(300);printf("%s", "无");
+
+
+
+system("color 9");Sleep(300);printf("%s", "忧\n");
+
+
+
+system("color 6");Sleep(300);printf("%s", "眉");
+
+
+
+system("color 1");Sleep(300);printf("%s", "眼");
+
+
+
+system("color 3");Sleep(300);printf("%s", "带");
+
+
+
+system("color 4");Sleep(300);printf("%s", "笑\n");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 2");Sleep(300);printf("%s", "祝");
+
+
+
+system("color 5");Sleep(300);printf("%s", "愿");
+
+
+
+system("color 9");Sleep(300);printf("%s", "你");
+
+
+
+system("color 6");Sleep(300);printf("%s", "平");
+
+
+
+system("color 1");Sleep(300);printf("%s", "安");
+
+
+
+system("color 3");Sleep(300);printf("%s", "喜");
+
+
+
+system("color 4");Sleep(300);printf("%s", "乐\n");//7字
+
+
+
+system("color 2");Sleep(300);printf("%s", " ");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 5");Sleep(300);printf("%s", "得");//4字
+
+
+
+system("color 9");Sleep(300);printf("%s", "偿");
+
+
+
+system("color 6");Sleep(300);printf("%s", "所");
+
+
+
+system("color 1");Sleep(300);printf("%s", "愿\n");
+
+
+
+Sleep(300);printf("%s", " ");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 4");Sleep(300);printf("%s", "everyone");
+
+
+
+system("color 2");Sleep(300);printf("%s", "祝");
+
+
+
+system("color 5");Sleep(300);printf("%s", "你");
+
+
+
+system("color 9");Sleep(300);printf("%s", "生");
+
+
+
+system("color 6");Sleep(300);printf("%s", "日");
+
+
+
+system("color 1");Sleep(300);printf("%s", "快");
+
+
+
+system("color 3");Sleep(300);printf("%s", "乐");
+
+
+
+system("color 4");Sleep(300);printf("%s", "与");
+
+
+
+system("color 2");Sleep(300);printf("%s", "安然\n");//9字
+
+
+
+system("color 5");Sleep(300);printf("%s", " ");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 9");Sleep(300);printf("%s", "我");//7
+
+
+
+system("color 6");Sleep(300);printf("%s", "只");
+
+
+
+system("color 1");Sleep(300);printf("%s", "愿");
+
+
+
+system("color 3");Sleep(300);printf("%s", "你");
+
+
+
+system("color 4");Sleep(300);printf("%s", "遍");
+
+
+
+system("color 2");Sleep(300);printf("%s", "历");
+
+
+
+system("color 5");Sleep(300);printf("%s", "山河\n");
+
+
+
+system("color 9");Sleep(300);printf("%s", " ");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 6");Sleep(300);printf("%s", "觉得");//4
+
+
+
+system("color 1");Sleep(300);printf("%s", "人");
+
+
+
+system("color 3");Sleep(300);printf("%s", "间");
+
+
+
+system("color 4");Sleep(300);printf("%s", "值得\n");
+
+
+
+Sleep(300);printf("%s", " ");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 2");Sleep(300);printf("%s", "至于");//9
+
+
+
+system("color 5");Sleep(300);printf("%s", "未来");
+
+
+
+system("color 9");Sleep(300);printf("%s", "会");
+
+
+
+system("color 6");Sleep(300);printf("%s", "怎样，");
+
+
+
+system("color 1");Sleep(300);printf("%s", "用力");
+
+
+
+system("color 3");Sleep(300);printf("%s", "走下去");
+
+
+
+system("color 4");Sleep(300);printf("%s", "才");
+
+
+
+system("color 2");Sleep(300);printf("%s", "会");
+
+
+
+system("color 5");Sleep(300);printf("%s", "知道\n");
+
+
+
+Sleep(300);printf("%s", " ");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 9");Sleep(300);printf("%s", "记住");//7
+
+
+
+system("color 6");Sleep(300);printf("%s", "先");
+
+
+
+system("color 1");Sleep(300);printf("%s", "变成");
+
+
+
+system("color 3");Sleep(300);printf("%s", "自己");
+
+
+
+system("color 4");Sleep(300);printf("%s", "喜欢");
+
+
+
+system("color 2");Sleep(300);printf("%s", "的样子\n");
+
+
+
+Sleep(300);printf("%s", " ");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 5");Sleep(300);printf("%s", "生而");//4
+
+
+
+system("color 9");Sleep(300);printf("%s", "无畏");
+
+
+
+system("color 6");Sleep(300);printf("%s", "战至");
+
+
+
+system("color 1");Sleep(300);printf("%s", "终章！\n");
+
+
+
+Sleep(300);printf("%s", " ");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 3");Sleep(300);printf("%s", "愿你");//9
+
+
+
+system("color 4");Sleep(300);printf("%s", "做到");
+
+
+
+system("color 2");Sleep(300);printf("%s", "既温柔");
+
+
+
+system("color 5");Sleep(300);printf("%s", "又强硬");
+
+
+
+system("color 9");Sleep(300);printf("%s", "适度敏感");
+
+
+
+system("color 6");Sleep(300);printf("%s", "使生活充实");
+
+
+
+system("color 1");Sleep(300);printf("%s", "适度粗糙");
+
+
+
+system("color 3");Sleep(300);printf("%s", "心墙才愈加");
+
+
+
+system("color 4");Sleep(300);printf("%s", "坚强\n");
+
+
+
+Sleep(300);printf("%s", " ");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 2");Sleep(300);printf("%s", "愿你");//7
+
+
+
+system("color 5");Sleep(300);printf("%s", "将爱");
+
+
+
+system("color 9");Sleep(300);printf("%s", "留给");
+
+
+
+system("color 6");Sleep(300);printf("%s", "心系");
+
+
+
+system("color 1");Sleep(300);printf("%s", "之");
+
+
+
+system("color 3");Sleep(300);printf("%s", "人\n");
+
+
+
+Sleep(300);printf("%s", " ");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 4");Sleep(300);printf("%s", "温柔");//4
+
+
+
+system("color 2");Sleep(300);printf("%s", "馈赠四方");
+
+
+
+system("color 5");Sleep(300);printf("%s", "灵魂");
+
+
+
+system("color 9");Sleep(300);printf("%s", "独赏\n");
+
+
+
+Sleep(300);printf("%s", " ");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 6");Sleep(300);printf("%s", "愿你");
+
+
+
+system("color 1");Sleep(300);printf("%s", "生在");//9
+
+
+
+system("color 3");Sleep(300);printf("%s", "这乏味世间");
+
+
+
+system("color 4");Sleep(300);printf("%s", "做些");
+
+
+
+system("color 2");Sleep(300);printf("%s", "喜爱");
+
+
+
+system("color 5");Sleep(300);printf("%s", "之事");
+
+
+
+system("color 9");Sleep(300);printf("%s", "欢度");
+
+
+
+system("color 6");Sleep(300);printf("%s", "岁月");
+
+
+
+system("color 1");Sleep(300);printf("%s", "才是\n");
+
+
+
+Sleep(300);printf("%s", " ");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 3");Sleep(300);printf("%s", "朝着");//7
+
+
+
+system("color 4");Sleep(300);printf("%s", "既定");
+
+
+
+system("color 2");Sleep(300);printf("%s", "方向");
+
+
+
+system("color 5");Sleep(300);printf("%s", "去");
+
+
+
+system("color 9");Sleep(300);printf("%s", "努力");
+
+
+
+system("color 6");Sleep(300);printf("%s", "天赋取三");
+
+
+
+system("color 1");Sleep(300);printf("%s", "成就不凡\n");
+
+
+
+Sleep(300);printf("%s", " ");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 3"); Sleep(300);printf("%s", "愿你");//7
+
+
+
+system("color 4");Sleep(300);printf("%s", "接受");
+
+
+
+system("color 2");Sleep(300);printf("%s", "寂寞，");
+
+
+
+system("color 5");Sleep(300);printf("%s", "那是");
+
+
+
+system("color 9");Sleep(300);printf("%s", "一个人");
+
+
+
+system("color 6");Sleep(300);printf("%s", "的");
+
+
+
+system("color 1");Sleep(300);printf("%s", "丰富\n");
+
+
+
+system("color 3");Sleep(300);printf("%s", " ");
+
+
+
+system("color 4");Sleep(300);printf("%s", "");
+
+
+
+system("color 2");Sleep(300);printf("%s", "");
+
+
+
+system("color 5");Sleep(stoptimelong);printf("%s", "");
+
+
+
+system("color 9");Sleep(stoptimelong);printf("%s", "");
+
+
+
+system("color 6");Sleep(stoptimelong);printf("%s", "");
+
+
+
+system("color 1");printf("%s", " ");system("color 3");
+
+
+
+system("color 3");Sleep(300);printf("%s", "热闹");//2
+
+
+
+system("color 4");Sleep(300);printf("%s", "只是\n");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 2");
+
+
+
+Sleep(300);printf("%s", "集体的孤独");//1
+
+
+
+system("color 5");Sleep(300);printf("%s", "");//6
+
+
+
+system("color 9");Sleep(300);printf("%s", "");
+
+
+
+system("color 6");Sleep(300);printf("%s", "");
+
+
+
+system("color 1");Sleep(300);printf("%s", "");
+
+
+
+system("color 3");Sleep(300);printf("%s", "");
+
+
+
+system("color 4");Sleep(300);printf("%s", "\n");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 2"); Sleep(300);printf("%s", "愿你");//7
+
+
+
+system("color 5");Sleep(300);printf("%s", "一边");
+
+
+
+system("color 9");Sleep(300);printf("%s", "踉跄");
+
+
+
+system("color 6");Sleep(300);printf("%s", "前行\n");
+
+
+
+system("color 1");Sleep(300);printf("%s", "一边");
+
+
+
+system("color 3");Sleep(300);printf("%s", "重振");
+
+
+
+system("color 4");Sleep(300);printf("%s", "旗鼓\n");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 2");Sleep(300);printf("%s", "愿你");//5
+
+
+
+system("color 5");Sleep(300);printf("%s", "尝尽");
+
+
+
+system("color 9");Sleep(300);printf("%s", "人生");
+
+
+
+system("color 6");Sleep(300);printf("%s", "那");
+
+
+
+system("color 1");Sleep(300);printf("%s", "百味\n");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 3");Sleep(300);printf("%s", "体味");//5
+
+
+
+system("color 4");Sleep(300);printf("%s", "那");
+
+
+
+system("color 2");Sleep(300);printf("%s", "人间");
+
+
+
+system("color 5");Sleep(300);printf("%s", "的");
+
+
+
+system("color 9");Sleep(300);printf("%s", "冷暖\n");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 6");Sleep(300);printf("%s", "愿你");//5
+
+
+
+system("color 1");Sleep(300);printf("%s", "闭上");
+
+
+
+system("color 3");Sleep(300);printf("%s", "双眼 ");
+
+
+
+system("color 4");Sleep(300);printf("%s", "是那");
+
+
+
+system("color 2");Sleep(300);printf("%s", "暴风作伴\n");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 5");Sleep(stoptimelong);printf("%s", "********");
+
+
+
+system("color 9");Sleep(stoptimelong);printf("%s", "********\n");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 6");Sleep(200);printf("%s", "你今日");//5
+
+
+
+system("color 1");Sleep(200);printf("%s", "一切");
+
+
+
+system("color 3");Sleep(200);printf("%s", " 我也");
+
+
+
+system("color 4");Sleep(200);printf("%s", "见证");
+
+
+
+system("color 2");Sleep(200);printf("%s", "许多\n");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 5");Sleep(200);printf("%s", "坎");//8
+
+
+
+system("color 9");Sleep(200);printf("%s", "很多");
+
+
+
+system("color 6");Sleep(200);printf("%s", "也很难");
+
+
+
+system("color 1");Sleep(200);printf("%s", " 你总是");
+
+
+
+system("color 3");Sleep(200);printf("%s", "苦笑");
+
+
+
+system("color 4");Sleep(200);printf("%s", " 但 ");
+
+
+
+system("color 2");Sleep(200);printf("%s", "依旧热情");
+
+
+
+system("color 5");Sleep(200);printf("%s", "这就是你\n");
+
+
+
+printf("%s", " ");
+
+
+
+system("color 9");Sleep(200);printf("%s", "从未");//4
+
+
+
+system("color 6");Sleep(200);printf("%s", "惧怕失败\n");
+
+
+
+system("color 1");Sleep(200);printf("%s", "你明白");
+
+
+
+system("color 3");Sleep(200);printf("%s", "自己一定会赢");
+
+
+
+Sleep(200);printf("%s", " ");
+
+
+
+system("color 5");Sleep(400);printf("%s", "愿友谊长久");//2
+
+system("color 1");Sleep(200);printf("%s", "但使龙城飞将在");
+
+system("color 1");Sleep(200);printf("%s", "不教胡马度阴山\n");
+
+
+
+system("color 2");Sleep(400);printf("%s", "郑琪");
+
+
+
+system("color 4");Sleep(400);printf("%s", " 要say32 bye! 20 Hi! ");
+
+
+
+system("pause"); return 0;
+
+
+
+}
